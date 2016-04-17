853a5a35ec96de5e38fcf1647f1bcfbaa563b61c
Aditya <ladwa.aditya@gmail.com>
2016-04-17 22:30:19 +0530

    refactor: Added contorller logic to display dummy categories;

--- public/modules/categories/controllers/categories.client.controller.js
+++ public/modules/categories/controllers/categories.client.controller.js
@@ -1,8 +1,28 @@
 'use strict';
 
-angular.module('categories').controller('CategoriesController', ['$scope',
-	function($scope) {
-		// Controller Logic
-		// ...
+angular.module('categories').controller('CategoriesController', ['$scope','$location',
+	function($scope,$location) {
+
+		// Create new Category
+		$scope.create = function() {
+			// Redirect after save
+			$location.path('categories');
+
+			// Clear form fields
+			$scope.name = '';
+		};
+
+		// Find a list of Categories
+		$scope.find = function() {
+			// hard coded data
+			$scope.categories = [{
+				'name': 'Beverages',
+				'description': 'Soft drinks, coffees, teas, beers, and ales'
+			},
+				{
+					'name': 'Condiments',
+					'description': 'Sweet and savory sauces, relishes, spreads, and seasonings'
+				}];
+		};
 	}
 ]);
