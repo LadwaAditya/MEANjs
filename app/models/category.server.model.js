abf6ee1747066a63ce85d6e2bc10869a5827e84d
Aditya <ladwa.aditya@gmail.com>
2016-04-17 20:19:55 +0530

    refactor: Added properties to module and tested;

--- app/models/category.server.model.js
+++ app/models/category.server.model.js
@@ -7,11 +7,34 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 /**
+ * Validation
+ */
+function validateLength (v) {
+	// a custom validation function for checking string length to be used by the model
+	return v.length <= 15;
+}
+
+
+/**
  * Category Schema
  */
 var CategorySchema = new Schema({
-	// Category model fields   
-	// ...
+	created:{
+		type:Date,
+		default:Date.now
+	},
+	description:{
+		type:String,
+		default:'',
+		trim:true
+	},
+	name:{
+		type:String,
+		default:'',
+		trim:true,
+		required: 'Name cannot be blank',
+		validate:[validateLength,'Name must be 15 chars in length or less']
+	}
 });
 
 mongoose.model('Category', CategorySchema);
--- app/tests/category.server.model.test.js
+++ app/tests/category.server.model.test.js
@@ -4,9 +4,8 @@
  * Module dependencies.
  */
 var should = require('should'),
-	mongoose = require('mongoose'),
-	User = mongoose.model('User'),
-	Category = mongoose.model('Category');
+    mongoose = require('mongoose'),
+    Category = mongoose.model('Category');
 
 /**
  * Globals
@@ -17,14 +16,59 @@ var user, category;
  * Unit tests
  */
 
-describe('Category Model',function () {
-	describe('Saving',function () {
-		it('saves new record');
+describe('Category Model', function () {
+    describe('Saving', function () {
+        it('saves new record', function (done) {
+            var category = new Category({
+                name: 'Beverages',
+                description: 'Soft drinks, coffees, teas, beers, and ales'
+            });
 
-		it('throws validation error when name is empty');
+            category.save(function (err, saved) {
+                should.not.exist(err);
+                done();
+            });
+        });
 
-		it('throws validation error when name longer than 15 chars');
+        it('throws validation error when name is empty', function () {
+            var category = new Category({
+                description: 'Soft drinks, coffees, teas, beers, and ales'
+            });
 
-		it('throws validation error for duplicate category name');
-	})
+            category.save(function (err) {
+                should.exist(err);
+                err.errors.name.message.should.equal('name cannot be blank');
+                done();
+            });
+        });
+
+        it('throws validation error when name longer than 15 chars', function () {
+            var category = new Category({
+                name: 'Grains/Cereals/Chocolates'
+            });
+            category.save(function (err, saved) {
+                should.exist(err);
+                err.errors.name.message.should.equal('Name must be 15 chars in length or less');
+                done();
+            });
+        });
+
+        it('throws validation error for duplicate category name', function (done) {
+            var category = new Category({
+                name: 'Beverages'
+            });
+
+            category.save(function (err) {
+                should.not.exist(err);
+                done();
+
+            });
+        });
+    });
+
+    afterEach(function (done) {
+        // NB this deletes ALL categories (but is run against a test database)
+        Category.remove().exec();
+        done();
+    });
 });
